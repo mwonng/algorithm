713afe9499ccb8c68a40a495836b930b30b80e0b
Michael Wonng <michael@wonng.com>
2019-05-08 19:02:35 +1000

    add 215

---
+++ 215.KthLargestElement.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    nums.sort( (a, b) => a-b);
+    return nums[nums.length - k];
+};
+
+
+console.log(
+    findKthLargest(
+        [3,2,1,5,6,4],
+        2
+    )
+);
+
+console.log(
+    findKthLargest(
+        [3,2,3,1,2,4,5,5,6],
+        4
+    )
+);
